ede8fa503cd04438eb285c334c480add84633d89
Kyle Mathews <mathews.kyle@gmail.com>
2025-02-03 15:14:17 -0700

    Sketch out Durable Object setup

--- sst.config.ts
+++ sst.config.ts
@@ -15,7 +15,7 @@ export default $config({
     };
   },
   async run() {
-    const { getNeonConnectionString, createNeonDb } = await import('./neon');
+    const { getNeonConnectionString, createNeonDb } = await import("./neon");
     // Create a db in Neon
     const project = neon.getProjectOutput({ id: `square-flower-52864146` });
     const dbName = `user_benchmark_${$app.stage.replace(/-/g, `_`)}`;
@@ -46,13 +46,30 @@ export default $config({
     });
 
     // TODO run db migrations here
+    //
+    // TODO add db as source to Cloud
 
-    const vpc = sst.aws.Vpc.get("examples-infra-shared-examplesInfraVpcShared", "vpc-044836d73fc26a218");
+    const vpc = sst.aws.Vpc.get(
+      "examples-infra-shared-examplesInfraVpcShared",
+      "vpc-044836d73fc26a218",
+    );
     const redis = new sst.aws.Redis("redis", { vpc });
 
-    const cluster = sst.aws.Cluster.get("examples-infra-shared-examplesInfraClusterSharedCluster", { vpc, id: `arn:aws:ecs:us-east-1:904233135193:cluster/examples-infra-shared-examplesInfraClusterSharedCluster` });
+    const cluster = sst.aws.Cluster.get(
+      "examples-infra-shared-examplesInfraClusterSharedCluster",
+      {
+        vpc,
+        id: `arn:aws:ecs:us-east-1:904233135193:cluster/examples-infra-shared-examplesInfraClusterSharedCluster`,
+      },
+    );
 
-    // TODO create a durable object as a wrangler service — SST doesn't support DOs yet unfortunately.
+    // Run durable object as a wrangler service — SST doesn't support DOs yet unfortunately.
+    new sst.x.DevCommand(`wrangler`, {
+      dev: {
+        command: `npx wrangler dev --var ELECTRIC_URL:TODO still`,
+        title: `Wrangler`,
+      },
+    });
 
     const nodejs = cluster.addService("nodejs", {
       loadBalancer: {
